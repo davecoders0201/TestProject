5f303dabd9fa26a483a52a4905f62652211c5209
davecoders0201 <davecoder0201@gmail.com>
2023-05-08 15:15:51 +0530

    Optimize Code

--- Screen/paymentScreen/CheckoutForm.js
+++ Screen/paymentScreen/CheckoutForm.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-// import axios from 'axios';
 import {
   StripeProvider,
   CardField,
